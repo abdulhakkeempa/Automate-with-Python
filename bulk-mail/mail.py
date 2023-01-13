7ecdd9d3e710975f60f2e16dae1c4d42a0b56c6a
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-01-13 21:41:13 +0530

    mail working

--- bulk-mail/mail.py
+++ bulk-mail/mail.py
@@ -3,18 +3,33 @@ from os.path import basename
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.utils import COMMASPACE, formatdate
+from email.utils import formatdate
 import pandas as pd;
 from dotenv import load_dotenv
 import os
 
-#function to send email
-def send_mail(send_from, send_to, subject, text, files=None,server="127.0.0.1"):
-    assert isinstance(send_to, list)
+#list to hold the details of failed communications
+failed = []
 
+#email body
+body = """Respected sir , 
+
+Thank you for attending the Pre University Capacity Building Workshop on Artificial Intelligence .  Here is your Participation Certificate.
+
+With Regards
+Organiser
+Pre University Capacity Building Workshop
+"""
+
+#email subject
+subject = "Certificate of Participation - IEEE Pre - University Capacity Building Workshop"
+
+
+#function to send email
+def send_mail(send_from, send_to, subject, text, files=None):
     msg = MIMEMultipart()
     msg['From'] = send_from
-    msg['To'] = COMMASPACE.join(send_to)
+    msg['To'] = send_to
     msg['Date'] = formatdate(localtime=True)
     msg['Subject'] = subject
 
@@ -25,36 +40,41 @@ def send_mail(send_from, send_to, subject, text, files=None,server="127.0.0.1"):
               fil.read(),
               Name=basename(files)
       )
+
     # After the file is closed
     part['Content-Disposition'] = 'attachment; filename="%s"' % basename(files)
     msg.attach(part)
 
+    #catching the exception incase of invalid email id
+    try:
+      smtp.sendmail(send_from, send_to, msg.as_string())
+    except:
+      failed.append(send_to)
 
-    smtp = smtplib.SMTP(server)
-    smtp.sendmail(send_from, send_to, msg.as_string())
-    smtp.close()
-
-
+#loading env variables
 load_dotenv()
 
 #smtp email configuration
-smtp = smtplib.SMTP(os.getenv('SMTP_SERVER'), os.getenv('SMTP_PORT'))
+smtp = smtplib.SMTP()
+smtp._host = os.getenv('SMTP_SERVER')
+smtp.connect(os.getenv('SMTP_SERVER'), os.getenv('SMTP_PORT'))
 smtp.starttls()
 smtp.login(os.getenv('FROM_EMAIL'), os.getenv('PASSWORD'))
 
+#reading the dataset
 df = pd.read_csv("assets/data/data.csv")
 
+#iterating and sending the mail
 for index,col in df.iterrows():
   print(col['Name']+":"+col['Email'])
+  #attachment file
+  file = "assets/"+col['Name'].rstrip()+".pdf"
+  send_mail(os.getenv('FROM_EMAIL'),col['Email'].rstrip(),subject,body,file)
+
+
+print("Failed Mails:"+failed)
 
 
 
-subject = """Respected sir , 
 
-Thank you for attending the Pre University Capacity Building Workshop on Artificial Intelligence .  Here is your Participation Certificate.
-
-With Regards
-Organiser
-Pre University Capacity Building Workshop
-"""
 
